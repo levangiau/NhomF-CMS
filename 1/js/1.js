5c7dc054ed2b4bbdb3b9d7f43acea1c017862a4d
levangiau <33826428+levangiau@users.noreply.github.com>
2019-11-03 20:44:45 +0700

    update-module1

--- 1/js/1.js
+++ 1/js/1.js
@@ -8,4 +8,29 @@ $(document).ready(function(){
 			$('.scroll').removeClass('hh');
 		}
 	});
+	$('.down').click(function(){
+		$('.dropdown-menu').addClass('a4');
+		$('.up').css({'display':'block'});
+		$('.down').css({'display':'none'});
+	});
+	$('.up').click(function(){
+		$('.dropdown-menu').removeClass('a4');
+		$('.up').css({'display':'none'});
+		$('.down').css({'display':'block'});
+	});
+	//bo class
+	$('.dropdown').hover(function(){
+		$(this).removeClass('open');
+
+		$('.dropdown-menu').css({'display':'none'});
+		//$(this).removeClass('dropdown');
+		//$(this).removeClass('show-hover');
+
+	});
+	$('.dropdown').mouseover(function(){
+		$('.dropdown').hover(function(){
+			$('.aaa').css({'display':'block !important','visibility':'visible','opacity':1});
+		});
+	});
+
 });
